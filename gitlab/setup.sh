96b45a8844f8ceb9aa5aa99378615ebbcb2b6f6a
Palmer Sample <40956652+palmersample@users.noreply.github.com>
2024-04-11 18:32:36 -0400

    Update setup script to remove zombie GitLab runners (#2)

--- gitlab/setup.sh
+++ gitlab/setup.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 gitlab_host="http://10.0.0.102"
+gitlab_token="CiscoAutomationUser"
+runner_name="runner1"
+runner_token="sandboxrunners"
 gitlab_wait_time=45
+
 # prints colored text
 success () {
     COLOR="92m"; # green
@@ -18,6 +22,56 @@ success
 printf "Waiting for Gitlab CE to become available..."
 
 until $(curl --output /dev/null --silent --head --fail ${gitlab_host}); do
+    printf '.'
+    sleep 10
+done
+success
+
+echo ""
+printf "Removing any stale runners..."
+RUNNERS=$(/usr/bin/curl --silent \
+            --request GET \
+            --header "PRIVATE-TOKEN: ${gitlab_token}" \
+            ${gitlab_host}/api/v4/runners/all | \
+            /usr/bin/jq '.[].id')
+
+for RUNNER in ${RUNNERS}; do
+    /usr/bin/curl --request DELETE \
+        --header "PRIVATE-TOKEN: ${gitlab_token}" \
+        ${gitlab_host}/api/v4/runners/${RUNNER}
+done
+success
+
+printf "Getting runner container name..."
+runner_container=$(/usr/bin/docker compose ps --no-trunc --format=json | \
+                       jq -s ".[] | select(.Service == \"${runner_name}\") | .Name" | \
+                       tr -d '"')
+success
+
+printf "Registering CICD Runner..."
+/usr/bin/docker exec -d ${runner_container} /usr/bin/gitlab-runner register \
+  --url ${gitlab_host} \
+  -r ${runner_token}
+sleep 5
+/usr/bin/docker exec -d ${runner_container} /usr/bin/gitlab-runner verify \
+  --url ${gitlab_host} \
+  -n ${runner_name} \
+  --delete
+success
+
+printf "Waiting for runner to start..."
+health_check_result=$(/usr/bin/curl --silent \
+                          --request GET \
+                          --header "PRIVATE-TOKEN: ${gitlab_token}" \
+                          ${gitlab_host}/api/v4/runners/all | \
+                          /usr/bin/jq '.[].status')
+
+until [ $health_check_result = '"online"' ]; do
+    health_check_result=$(/usr/bin/curl --silent \
+                            --request GET \
+                            --header "PRIVATE-TOKEN: ${gitlab_token}" \
+                            ${gitlab_host}/api/v4/runners/all | \
+                            /usr/bin/jq '.[].status')
     printf '.'
     sleep 10
 done
